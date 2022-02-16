c817f8eb41c043d388481ec39b1df14ba6eaa12a
lisa6125 <fxck6125@gmail.com>
2022-02-16 14:38:54 +0800

    設置 input 的雙向綁定

--- src/pages/Home/components/Edit.js
+++ src/pages/Home/components/Edit.js
@@ -1,15 +1,32 @@
+import { useState } from 'react'
+
 const Edit = ({ add }) => {
+  const [note, setNote] = useState("")
+  function noteChange(e) {
+    setNote(e.target.value)
+  }
+  const [date, setDate] = useState("")
+  function dateChange(e) {
+    setDate(e.target.value)
+  }
+  const [time, setTime] = useState("")
+  function timeChange(e) {
+    setTime(e.target.value)
+  }
+
   function addItem() {
     add(['改變傳入的data'])
   }
+
+
   return <div>
     <h1>備忘錄</h1>
     <p>記事:</p>
-    <input type="text" />
+    <input type="text" value={note} onChange={ noteChange }/>
     <p>日期:</p>
-    <input type="date" />
+    <input type="date" value={date} onChange={ dateChange }/>
     <p>時間:</p>
-    <input type="time" />
+    <input type="time" value={time} onChange={ timeChange }/>
     <button className="add" onClick={ addItem }>新增</button>
   </div>
 }
